fdd26b6de4c67897936814ae5e4ad7a0234125ac
Roger Terrill <lpaddikt@gmail.com>
2020-06-23 15:40:55 -0700

    Fixed the sort to be first by time, then by title

--- cst438assignment1/src/main/java/com/example/cst438assignment1/MovieRepository.java
+++ cst438assignment1/src/main/java/com/example/cst438assignment1/MovieRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface MovieRepository extends CrudRepository<Movie, Long>{
 
-  @Query("select m from Movie m order by movieTitle asc, time desc")
+//  @Query("select m from Movie m order by movieTitle asc, time desc")
+  @Query(value = "SELECT * from Movie order by time DESC, movie_title ASC", nativeQuery=true)
   List<Movie> findAllMoviesOrderByTitleDateDesc();
 
 }
